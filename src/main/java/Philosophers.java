84c89a24f648bd2759ff900a1b10c952633e4f8a
IurieStanila <stanilaiurie@gmail.com>
2022-11-20 13:08:49 +0200

    Thinker 3

--- src/main/java/Philosophers.java
+++ src/main/java/Philosophers.java
@@ -1,5 +1,11 @@
+
 public class Philosophers {
+
     public static void main(String[] args) {
+
+        String thinker = "Sartre";
+        String thinker2 = "";
+        String thinker3 = "";
 
     }
 }
